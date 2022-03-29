8c19fce208d5ece6c8a467d1c59db3b4d786fdf6
Zach Wahrer <zach.wahrer@foundant.com>
2022-03-29 10:05:21 -0600

    Cleanup

--- .config/nvim/lua/color_config.lua
+++ .config/nvim/lua/color_config.lua
@@ -1,7 +1,7 @@
 vim.cmd([[
 set termguicolors
 let g:onedark_config = { 'style': 'warmer' }
-colorscheme onedark
+colorscheme onedark 
 hi Search guibg=Cyan
 hi SpellBad guibg=gray20 guifg=Orange
 ]])
--- .config/nvim/lua/plugins_config.lua
+++ .config/nvim/lua/plugins_config.lua
@@ -1,34 +1,54 @@
 vim.cmd([[
 call plug#begin()
-Plug 'neovim/nvim-lspconfig' " LSP
-Plug 'ray-x/guihua.lua', {'do': 'cd lua/fzy && make' } " Navigator dep
-Plug 'ray-x/navigator.lua' " Navigator
-Plug 'navarasu/onedark.nvim' " Theme
-Plug 'Th3Whit3Wolf/one-nvim' " Theme
-Plug 'yonlu/omni.vim' " Theme
-Plug 'nvim-lualine/lualine.nvim'  " Status Bar
+
+"""""" THEMES """"""
+Plug 'navarasu/onedark.nvim'
+Plug 'Th3Whit3Wolf/one-nvim'
+Plug 'marko-cerovac/material.nvim'
+Plug 'yonlu/omni.vim' 
+Plug 'sainnhe/edge' 
+Plug 'PHSix/nvim-hybrid'
+Plug 'olimorris/onedarkpro.nvim'
+Plug 'rmehri01/onenord.nvim'
+
+"""""" REQS """"""
 Plug 'kyazdani42/nvim-web-devicons'  " Lualine, Barbar, vgit, nvim-tree
-Plug 'akinsho/bufferline.nvim' " Buffer management
-Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'} " Treesitter
-Plug 'fatih/vim-go', { 'do': ':GoUpdateBinaries' } " Go
-Plug 'sebdah/vim-delve' " Delve
-Plug 'tpope/vim-dotenv' " Env management
-Plug 'buoto/gotests-vim' " Go Tests
-" Plug 'hexdigest/gounit-vim' " Go Tests / Allows templates to be created
-Plug 'vim-test/vim-test' " Vim-test
-Plug 'tpope/vim-dispatch' " Vim-test dep
-Plug 'skywind3000/asyncrun.vim' " Vim-test dep
-Plug 'ahmedkhalf/project.nvim' " Project management
 Plug 'nvim-lua/plenary.nvim' " Telescope, vgit, todo-comments req
-Plug 'tanvirtin/vgit.nvim' "Vgit
-Plug 'kdheepak/lazygit.nvim' " Lazy Git
-Plug 'nvim-telescope/telescope.nvim' " Finder
 Plug 'nvim-telescope/telescope-fzf-native.nvim', { 'do': 'make' } " Telescope req
+Plug 'jose-elias-alvarez/null-ls.nvim' " Prettier req
+Plug 'vim-denops/denops.vim' " Req for denops-docker
+
+"""""" LSP """"""
+Plug 'neovim/nvim-lspconfig' 
+Plug 'folke/lsp-colors.nvim' " Add missing lsp colors
+Plug 'ray-x/lsp_signature.nvim' " Show func signature
+
+"""""" INTERFACE """"""
+Plug 'nvim-lualine/lualine.nvim'  " Status Bar
+Plug 'akinsho/bufferline.nvim' " Buffer management
+Plug 'ahmedkhalf/project.nvim' " Project management
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'} " Treesitter
+Plug 'nvim-telescope/telescope.nvim' " Finder
 Plug 'b3nj5m1n/kommentary' " Comment
 Plug 'windwp/nvim-autopairs' " Auto pairs
 Plug 'dstein64/nvim-scrollview', { 'branch': 'main' } " Scroll bar
 Plug 'kyazdani42/nvim-tree.lua' " File browser
 Plug 'p00f/nvim-ts-rainbow' " Rainbow brackets
+Plug 'mg979/vim-visual-multi' " Multi cursors
+Plug 'psliwka/vim-smoothie' " Smooth scrolling
+Plug 'folke/trouble.nvim' " Pretty list for diagnostics
+Plug 'moll/vim-bbye' " Better buffer quit
+Plug 'abecodes/tabout.nvim' " Tab out
+Plug 'preservim/vimux' " Vimux for tests
+Plug 'lewis6991/spellsitter.nvim' " Spell check with treesitter
+Plug 'glepnir/dashboard-nvim' " Startup dashboard
+Plug 'luukvbaal/stabilize.nvim' " Window stabilize
+Plug 'folke/todo-comments.nvim' " Todo comment highlights
+Plug 'gpanders/editorconfig.nvim' " Use .editorconfig files
+Plug 'weilbith/nvim-code-action-menu' " Code action menu
+Plug 'appelgriebsch/surround.nvim' " Surround with chars
+
+"""""" COMPLETIONS """"""
 Plug 'hrsh7th/cmp-nvim-lsp' " Completions
 Plug 'hrsh7th/cmp-buffer' " Completions
 Plug 'hrsh7th/cmp-path' " Completions
@@ -36,30 +56,36 @@ Plug 'hrsh7th/cmp-cmdline' " Completions
 Plug 'hrsh7th/nvim-cmp' " Completions
 Plug 'hrsh7th/cmp-vsnip' " Snips
 Plug 'hrsh7th/vim-vsnip' " Snips
-Plug 'mg979/vim-visual-multi' " Multi cursors
-Plug 'psliwka/vim-smoothie' " Smooth scrolling
-Plug 'folke/lsp-colors.nvim' " Add missing lsp colors
-Plug 'folke/trouble.nvim' " Pretty list for diagnostics
-Plug 'moll/vim-bbye' " Better buffer quit
-Plug 'abecodes/tabout.nvim' " Tab out
-Plug 'preservim/vimux' " Vimux for tests
-Plug 'jose-elias-alvarez/null-ls.nvim' " Prettier req
+
+"""""" LANGUAGES """"""
+Plug 'fatih/vim-go', { 'do': ':GoUpdateBinaries' } " Go
+Plug 'sebdah/vim-delve' " Delve
 Plug 'MunifTanjim/prettier.nvim' " Prettier formatting
-Plug 'lewis6991/spellsitter.nvim' " Spell check with treesitter
 Plug 'ckipp01/stylua-nvim' " Lua formatting
-Plug 'glepnir/dashboard-nvim' " Startup dashboard
-Plug 'luukvbaal/stabilize.nvim' " Window stabilize
-Plug 'ray-x/lsp_signature.nvim' " Show func signature
-Plug 'folke/todo-comments.nvim' " Todo comment highlights
-Plug 'David-Kunz/jester' " Jest test dap debugger
+
+"""""" DEBUG """"""
 Plug 'mfussenegger/nvim-dap' " Debug adapter
 Plug 'leoluz/nvim-dap-go' " Adapt delve to dap
+Plug 'David-Kunz/jester' " Jest test dap debugger
 Plug 'rcarriga/nvim-dap-ui' " UI for dap
-Plug 'gpanders/editorconfig.nvim' " Use .editorconfig files
-Plug 'weilbith/nvim-code-action-menu' " Code action menu
+
+"""""" TESTING """"""
+Plug 'buoto/gotests-vim'
+" Plug 'hexdigest/gounit-vim' " Go Tests / Allows templates to be created
+Plug 'vim-test/vim-test' " Vim-test
+Plug 'tpope/vim-dispatch' " Vim-test dep
+Plug 'skywind3000/asyncrun.vim' " Vim-test dep
+
+"""""" ENV MANAGEMENT """"""
+Plug 'tpope/vim-dotenv' 
+
+"""""" GIT """"""
+Plug 'tanvirtin/vgit.nvim' "Vgit
 Plug 'sindrets/diffview.nvim' " Git diff view
-Plug 'appelgriebsch/surround.nvim' " Surround with chars
-Plug 'vim-denops/denops.vim' " Req for denops-docker
+Plug 'kdheepak/lazygit.nvim' " Lazy Git
+
+"""""" DOCKER """"""
 Plug 'skanehira/denops-docker.vim' " Docker
+
 call plug#end()
 ]])
