913affc2dfe934a6cc9d8ed7d4887727e1223dd2
Zach Wahrer <zach.wahrer@foundant.com>
2022-05-05 16:15:20 -0600

    Add i3 and polybar

---
+++ .config/i3/launch_polybar.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+# polybar &
+
+
+# https://github.com/polybar/polybar/issues/763
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
--- .config/nvim/lua/color_config.lua
+++ .config/nvim/lua/color_config.lua
@@ -2,6 +2,6 @@ vim.cmd([[
 set termguicolors
 let g:onedark_config = { 'style': 'warmer' }
 colorscheme onedark 
-hi Search guibg=Cyan
+hi Search guibg=#aa03af
 hi SpellBad guibg=gray20 guifg=Orange
 ]])
