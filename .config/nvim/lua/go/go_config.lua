15adba61c89a5514bffcc6ad78183553bc23e922
Zach Wahrer <zach.wahrer@foundant.com>
2022-05-11 07:38:35 -0600

    Add icons on polybar

--- .config/nvim/lua/go/go_config.lua
+++ .config/nvim/lua/go/go_config.lua
@@ -6,3 +6,11 @@ vim.api.nvim_set_keymap("n", "<leader>gc", "<cmd>GoCoverageToggle<CR>", { norema
 vim.api.nvim_set_keymap("n", "<leader>gf", "<cmd>GoFillStruct<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>gt", "<cmd>GoTest<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>ga", "<cmd>GoAlternate<CR>", { noremap = true })
+
+vim.g["go_metalinter_autosave_enabled"] = {'gofmt', 'govet', 'revive', 'errcheck', 'deadcode', 'gosimple', 'ineffassign', 'staticcheck', 'structcheck', 'typecheck','unused', 'varcheck', 'bodyclose', 'dogsled', 'goconst', 'gocyclo', 'importas', 'rowserrcheck', 'sqlclosecheck', 'misspell', 'gosec'}
+vim.g["go_metalinter_command"] = 'golangci-lint'
+vim.g["go_metalinter_autosave"] = 1
+vim.g["go_fmt_command"] = 'goimports'
+vim.g["go_fmt_autosave"] = 1
+vim.g["go_mod_fmt_autosave"] = 1
+vim.g["go_imports_autosave"] = 1
