a249a323029169331bbbe0de6aaf5b149629ec36
Zach Wahrer <zach.wahrer@foundant.com>
2022-07-18 14:36:13 -0600

    Fix issue with ctrl+/ mapping

--- dotfiles/config/nvim/lua/mapping.lua
+++ dotfiles/config/nvim/lua/mapping.lua
@@ -72,8 +72,8 @@ set_keymap("n", "<leader>m", "<cmd>lua require('harpoon.mark').add_file()<CR>", 
 set_keymap("n", "<leader>h", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", opts)
 
 -- Kommentary --
-set_keymap("x", "<C-_>", "<Plug>kommentary_visual_default", opts)
-set_keymap("n", "<C-_>", "<Plug>kommentary_line_default", opts)
+set_keymap("x", "<C-/>", "<Plug>kommentary_visual_default", opts)
+set_keymap("n", "<C-/>", "<Plug>kommentary_line_default", opts)
 --set_keymap("n", "<C-_>", "<Plug>kommentary_motion_default", opts)
 
 -- Lazygit --
