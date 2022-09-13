91f46a1789bad0149c11b764c6fa32a422ae4012
Zach Wahrer <zach.wahrer@foundant.com>
2022-09-13 09:24:28 -0600

    Clean up i3/polybar config

--- dotfiles/config/i3/launch_polybar.sh
+++ dotfiles/config/i3/launch_polybar.sh
@@ -6,31 +6,25 @@
   killall -q polybar
   while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
   outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+  tray_postion=right
 
   {%@@ if profile == "red-wolf" @@%}
-  export MONITOR=eDP-1
-  export TRAY_POSITION=right
-  polybar --reload main </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+  MONITOR=HDMI2 TRAY_POSITION=$tray_position polybar --reload main </dev/null >/var/tmp/polybar.log 2>&1 200>&- &
   disown
-
   {%@@ else @@%}
   if $outputs == "eDP-1"; then
-    export MONITOR=eDP-1
-    export TRAY_POSITION=right
-    polybar --reload main </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+    MONITOR=eDP-1 TRAY_POSITION=$tray_position polybar --reload main </dev/null >/var/tmp/polybar.log 2>&1 200>&- &
     disown
   else
     tray_output=DP-2-1
     for m in $outputs; do
-      export MONITOR=$m
-      export TRAY_POSITION=none
       if [[ $m == $tray_output ]]; then
-        TRAY_POSITION=right
+        MONITOR=$m TRAY_POSITION=$tray_position polybar --reload main </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+      else
+        MONITOR=$m TRAY_POSITION=none polybar --reload main </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
       fi
-      polybar --reload main </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
       disown
       done
   fi
   {%@@ endif @@%}
-
 ) 200>/var/tmp/polybar-launch.lock
