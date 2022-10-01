be5ee7e7bf6ebd4386b8da5a58ac0fecee1906bb
Zach Wahrer <zach.wahrer@foundant.com>
2022-10-01 16:05:23 -0600

    Update bufferline and lualine configs

--- dotfiles/config/nvim/lua/plugins/bufferline.lua
+++ dotfiles/config/nvim/lua/plugins/bufferline.lua
@@ -1,18 +1,22 @@
 require("bufferline").setup({
-	options = {
-		max_name_length = 50,
-		diagnostics = "nvim_lsp",
-		offsets = {
-			{
-				filetype = "NvimTree",
-				text = "",
-				highlight = "Directory",
-				text_align = "left",
-			},
-		},
-		close_command = "Bdelete! %d",
-		right_mouse_command = "vertical sbuffer %d",
-		left_mouse_command = "buffer %d",
-		middle_mouse_command = "Bdelete! %d",
-	},
+  options = {
+    max_name_length = 50,
+    max_prefix_length = 20,
+    truncate_names = false,
+    separator_style = "slant",
+    diagnostics = "nvim_lsp",
+    sort_by = "directory",
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = "",
+        highlight = "Directory",
+        text_align = "left",
+      },
+    },
+    close_command = "Bdelete! %d",
+    right_mouse_command = "vertical sbuffer %d",
+    left_mouse_command = "buffer %d",
+    middle_mouse_command = "Bdelete! %d",
+  },
 })
--- dotfiles/config/nvim/lua/plugins/lualine.lua
+++ dotfiles/config/nvim/lua/plugins/lualine.lua
@@ -2,11 +2,15 @@ require("lualine").setup({
 	options = {
 		-- theme = "onedarkpro",
 		theme = "onedark",
+		globalstatus = true,
 	},
 	sections = {
-		lualine_c = {
-			"lsp_progress",
-		},
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = { { "filename", path = 3 } },
+		lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
 	},
 	extensions = { "nvim-dap-ui", "nvim-tree" },
 })
