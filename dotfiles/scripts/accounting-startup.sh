accfd6dab15760e5f8fc941e66e58c91840c0c13
Zach Wahrer <zach.wahrer@threatoptix.ai>
2022-11-18 08:17:53 -0700

    Tweaks/additions

--- dotfiles/scripts/accounting-startup.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/zsh
-kitty @ send_text --match title:"Backend" "make backend-up-full\x0d"
-kitty @ send_text --match title:"Frontend" "make frontend\x0d"
--- init.sh
+++ init.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
 ## Packages / Tools ##
-sudo pacman -Syy git kitty neovim zsh fd ripgrep fzf shellcheck lua-language-server bash-language-server yaml-language-server yamllint stylua go gopls python-pip ncdu bpytop
-yay -install nerd-fonts-complete wdisplay vale
+sudo pacman -Syy git kitty neovim zsh fd ripgrep fzf shellcheck lua-language-server bash-language-server yaml-language-server yamllint stylua go gopls docker docker-compose python-pip ncdu bpytop
+yay -install dbgate-bin nerd-fonts-complete wdisplay vale
 pip3 install pynvim
 /usr/lib/go/bin/go install github.com/kyoh86/richgo@latest
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.50.1
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.50.1
 sudo npm install -g dockerfile-language-server-nodejs
+
+ ## Docker Setup ##
+sudo systemctl enable docker && sudo systemctl daemon-reload
+sudo usermod -aG docker "$USER"
+# newgrp docker # Or reboot if still getting non-sudo docker permission errors
 
 ## Dotfiles ##
 echo "Generate ssh key and add to github. [Enter to continue...]"
