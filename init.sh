9be90048bf264b91d8e2e924706d133eab31c6e3
Zach Wahrer <zach.wahrer@threatoptix.ai>
2022-12-05 13:04:42 -0700

    Add json utils to init

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 GO_BIN=/usr/lib/go/bin/go
 
 ## Packages / Tools ##
-sudo pacman -Syy git base-devel kitty neovim zsh fd ripgrep fzf shellcheck lua-language-server bash-language-server yaml-language-server yamllint stylua go gopls docker docker-compose python-pip ncdu bpytop sad tk mako swaylock waybar wofi firefox gcc npm pulseaudio pulseaudio-alsa alsa-utils wtype wl-clipboard xorg-xwayland network-manager-applet
+sudo pacman -Syy git base-devel kitty neovim zsh fd ripgrep fzf shellcheck lua-language-server bash-language-server yaml-language-server yamllint stylua go gopls docker docker-compose python-pip ncdu bpytop sad tk mako swaylock waybar wofi firefox gcc npm pulseaudio pulseaudio-alsa alsa-utils wtype wl-clipboard xorg-xwayland network-manager-applet jq fx
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd ../ && rm -rf yay
 yay -S dbgate-bin nerd-fonts-complete wdisplays vale
 pip3 install pynvim
