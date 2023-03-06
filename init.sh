a0a5a96ac7f184a2bd44e36e90099971f4355ab7
Zach Wahrer <zach.wahrer@threatoptix.ai>
2023-03-06 16:51:56 -0700

    Updates for init script

--- init.sh
+++ init.sh
@@ -4,13 +4,13 @@ GO_BIN=/usr/lib/go/bin/go
 
 ## Packages / Tools ##
 sudo pacman -Syyu # Update
-sudo pacman -S ncdu bpytop sad tk mako swaylock waybar wofi firefox pulseaudio pulseaudio-alsa alsa-utils pamixer wtype wl-clipboard xorg-xwayland network-manager-applet brightnessctl playerctl arc-gtk-theme ntp && \
+sudo pacman -S ncdu bpytop sad tk mako swaylock waybar wofi firefox pulseaudio pulseaudio-alsa alsa-utils pamixer wtype wl-clipboard xorg-xwayland network-manager-applet brightnessctl playerctl pavucontrol arc-gtk-theme ntp thunar swaybg && \
     sudo systemctl enable ntpd && sudo systemctl start ntpd	# System
 sudo pacman -S kitty neovim zsh fd ripgrep fzf shellcheck lua-language-server bash-language-server yaml-language-server yamllint stylua go gopls # Terminal / Neovim
 sudo pacman -S git base-devel gcc docker docker-compose python-pip pyright npm jq fx eslint prettier # Dev tools
 
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd ../ && rm -rf yay
-yay -S nwg-displays nwg-launchers nerd-fonts-complete sudo-font-git swaync wlogout  # System
+yay -S nwg-displays nwg-launchers ttf-nerd-fonts-symbols-2048-em-mono ttf-sudo swaync wlogout  # System
 yay -S checkmake dbgate-bin dotenv-linter hadolint vale # Dev tools
 
 pip3 install beautysh gitlint pynvim setuptools # Dev tools
@@ -32,7 +32,7 @@ sudo usermod -aG docker "$USER"
 # newgrp docker # Or reboot if still getting non-sudo docker permission errors
 
 ## Dotfiles ##
-echo "Generate ssh key and add to github. [Enter to continue...]"
+echo "Generate ssh key and add to github. [Enter to continue...]"git@github.cloudoptix.io:Bayonet/agentless-server.git
 firefox https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent &
 read -r _
 
