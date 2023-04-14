9b7830f8e59883846152bee2a6642132ca86955f
Zach Wahrer <zach.wahrer@threatoptix.ai>
2023-04-14 07:51:43 -0600

    Update to show recording in lualine

--- dotfiles/config/nvim/lua/plugins/lualine.lua
+++ dotfiles/config/nvim/lua/plugins/lualine.lua
@@ -11,7 +11,18 @@ local M = {
 function M.config()
 	Colors = require("colors")
 
-	require("lualine").setup({
+	local lualine = require("lualine")
+
+	local function show_macro_recording()
+		local recording_register = vim.fn.reg_recording()
+		if recording_register == "" then
+			return ""
+		else
+			return "Recording @" .. recording_register
+		end
+	end
+
+	lualine.setup({
 		options = {
 			theme = "onedark",
 			globalstatus = true,
@@ -28,6 +39,11 @@ function M.config()
 				-- "b:gitsigns_blame_line",
 			},
 			lualine_x = {
+				{
+					"macro-recording",
+					fmt = show_macro_recording,
+					color = { fg = Colors.red },
+				},
 				{ "searchcount",                       color = { fg = Colors.yellow } },
 				{ "require'nvim-possession'.status()", color = { fg = Colors.green } },
 				-- "encoding",
@@ -38,6 +54,35 @@ function M.config()
 			lualine_z = { "location" },
 		},
 		extensions = { "nvim-dap-ui", "neo-tree", "man" },
+	})
+
+	vim.api.nvim_create_autocmd("RecordingEnter", {
+		callback = function()
+			lualine.refresh({
+				place = { "statusline" },
+			})
+		end,
+	})
+
+	vim.api.nvim_create_autocmd("RecordingLeave", {
+		callback = function()
+			-- This is going to seem really weird!
+			-- Instead of just calling refresh we need to wait a moment because of the nature of
+			-- `vim.fn.reg_recording`. If we tell lualine to refresh right now it actually will
+			-- still show a recording occuring because `vim.fn.reg_recording` hasn't emptied yet.
+			-- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
+			-- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
+			local timer = vim.loop.new_timer()
+			timer:start(
+				50,
+				0,
+				vim.schedule_wrap(function()
+					lualine.refresh({
+						place = { "statusline" },
+					})
+				end)
+			)
+		end,
 	})
 end
 
