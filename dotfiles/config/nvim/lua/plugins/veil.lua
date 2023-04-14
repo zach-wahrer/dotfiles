701b4b5cb33f8ab7751f923abab163ff127fb9fa
Zach Wahrer <zach.wahrer@threatoptix.ai>
2023-04-14 16:06:29 -0600

    Add dashboard animation

---
+++ dotfiles/config/nvim/lua/plugins/veil.lua
@@ -0,0 +1,43 @@
+local M = {
+	"willothy/veil.nvim",
+	dependencies = "nvim-lua/plenary.nvim",
+}
+
+function M.config()
+	Colors = require("colors")
+	local cfgPath = vim.fn.stdpath("config")
+
+	local slides = {
+		"/slides/dashboard.cat",
+		"/slides/dashboard2.cat",
+		"/slides/dashboard3.cat",
+		"/slides/dashboard3.cat",
+		"/slides/dashboard4.cat",
+		"/slides/dashboard5.cat",
+		"/slides/dashboard6.cat",
+		"/slides/dashboard7.cat",
+	}
+
+	texts = {}
+	for _, slide in pairs(slides) do
+		local f = io.open(cfgPath .. slide)
+		local text = {}
+		if f ~= nil then
+			for each in f:lines() do
+				text[#text + 1] = each
+			end
+		end
+		texts[#texts + 1] = text
+	end
+
+	local builtin = require("veil.builtin")
+	require("veil").setup({
+		sections = {
+			builtin.sections.animated(texts, {
+				hl = { fg = Colors.red },
+			}),
+		},
+	})
+end
+
+return M
