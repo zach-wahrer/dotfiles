5a1221afcaff226b70fd98bd0bbbfada874eed01
Zach Wahrer <zach.wahrer@threatoptix.ai>
2023-05-02 13:27:13 -0600

    Small font updates and remove old files

--- dotfiles/config/nvim/lua/plugins/neo_tree.lua
+++ dotfiles/config/nvim/lua/plugins/neo_tree.lua
@@ -13,7 +13,7 @@ function M.config()
 	vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
 	vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
 	vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-	vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+	vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 	-- NOTE: this is changed from v1.x, which used the old style of highlight groups
 	-- in the form "LspDiagnosticsSignWarning"
 
