d7c26f1f83557ed0fb1b7c61b69202c1af466d0f
Zach Wahrer <zach.wahrer@threatoptix.ai>
2023-07-19 09:41:36 -0600

    Update plugins and add missing rainbow delimiters plugin file

---
+++ dotfiles/config/nvim/lua/plugins/rainbow_delimiters.lua
@@ -0,0 +1,30 @@
+local M = {
+	url = "https://gitlab.com/HiPhish/rainbow-delimiters.nvim",
+	event = "VeryLazy",
+}
+
+function M.config()
+	local rainbow_delimiters = require("rainbow-delimiters")
+
+	vim.g.rainbow_delimiters = {
+		strategy = {
+			[""] = rainbow_delimiters.strategy["global"],
+			vim = rainbow_delimiters.strategy["local"],
+		},
+		query = {
+			[""] = "rainbow-delimiters",
+			lua = "rainbow-blocks",
+		},
+		highlight = {
+			"RainbowDelimiterRed",
+			"RainbowDelimiterYellow",
+			"RainbowDelimiterBlue",
+			"RainbowDelimiterOrange",
+			"RainbowDelimiterGreen",
+			"RainbowDelimiterViolet",
+			"RainbowDelimiterCyan",
+		},
+	}
+end
+
+return M
