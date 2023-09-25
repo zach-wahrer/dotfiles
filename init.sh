57d89f5a9f7a4ce8491ef70a44dc0bb42a5a6756
Zach Wahrer <zach.wahrer@netography.com>
2023-09-25 15:26:00 -0600

    Update neovim plugins

--- init.sh
+++ init.sh
@@ -4,8 +4,9 @@ GO_BIN=/usr/lib/go/bin/go
 
 ## Packages / Tools ##
 sudo pacman -Syyu # Update
-sudo pacman -S ncdu bpytop lsd bat git-delta sad tk mako swaylock waybar wofi firefox pulseaudio pulseaudio-alsa alsa-utils pamixer wtype wl-clipboard xorg-xwayland network-manager-applet brightnessctl playerctl pavucontrol arc-gtk-theme ntp thunar thunar-volman gvfs swaybg man && \
-    sudo systemctl enable ntpd && sudo systemctl start ntpd	# System
+sudo pacman -S ncdu bpytop lsd bat git-delta sad tk mako swaylock waybar wofi firefox pulseaudio pulseaudio-alsa alsa-utils pamixer wtype wl-clipboard xorg-xwayland network-manager-applet brightnessctl playerctl pavucontrol arc-gtk-theme ntp thunar thunar-volman gvfs swaybg man ufw && \
+    sudo systemctl enable --now ntpd && sudo systemctl enable --now ufw	# System
+sudo ufw enable && sudo ufw default deny # Firewall
 sudo pacman -S kitty neovim zsh fd ripgrep fzf shellcheck lua-language-server bash-language-server yaml-language-server yamllint stylua go gopls # Terminal / Neovim
 sudo pacman -S git base-devel gcc docker docker-compose python-pip pyright npm jq fx eslint prettier # Dev tools
 
