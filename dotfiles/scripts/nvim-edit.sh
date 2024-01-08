770bf9b9e445d0f1e2272732b3999ae35d8ee037
Zach Wahrer <zach.wahrer@netography.com>
2024-01-08 08:19:52 -0700

    Add foot config and update nvim plugins

--- dotfiles/scripts/nvim-edit.sh
+++ dotfiles/scripts/nvim-edit.sh
@@ -4,11 +4,13 @@ file=$(mktemp)
 
 wtype -M ctrl c -m ctrl
 sleep 0.1s
-wl-paste > "$file"
+wl-paste >"$file"
 
-wezterm start -- nvim "$file"
+# For some reason, wezterm will not stay open if wtype is called before it.
+# wezterm start -- nvim "$file"
+foot -- nvim "$file"
 
-wl-copy < "$file"
+wl-copy <"$file"
 wtype -M ctrl v -m ctrl
 
 rm "$file"
