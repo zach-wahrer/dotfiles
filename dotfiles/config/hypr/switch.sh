f75688a0ee86ca79bcb9612b050f1698e8b55150
Zach Wahrer <zach.wahrer@threatoptix.ai>
2024-02-05 16:17:36 -0700

    Additional hyprland config

--- dotfiles/config/hypr/switch.sh
+++ dotfiles/config/hypr/switch.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 if grep open /proc/acpi/button/lid/LID0/state; then
+	{%@@ if profile == "neto-wolf" @@%}
 	hyprctl keyword monitor "eDP-1, 1920x1080@144.003006, 0x1920, 1"
+	{%@@ endif @@%}
+	{%@@ if profile == "red-wolf" @@%}
+	hyprctl keyword monitor "eDP-1, 1920x1080@60.029999, 0x0, 1.5"
+	{%@@ endif @@%}
 else
 	if [[ $(hyprctl monitors | grep "Monitor" | wc -l) != 1 ]]; then
 		hyprctl keyword monitor "eDP-1, disable"
