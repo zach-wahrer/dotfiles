3aa44cc9d98d64e2b81304fa1e9bb02d93d45459
Zach Wahrer <zach.wahrer@netography.com>
2024-02-28 10:52:43 -0700

    Tmux/Alacritty: Updates for layout

--- dotfiles/scripts/nvim-edit.sh
+++ dotfiles/scripts/nvim-edit.sh
@@ -6,9 +6,7 @@ wtype -M ctrl c -m ctrl
 sleep 0.1s
 wl-paste >"$file"
 
-# For some reason, wezterm will not stay open if wtype is called before it.
-# wezterm start -- nvim "$file"
-foot -- nvim "$file"
+alacritty -e nvim "$file"
 
 wl-copy <"$file"
 wtype -M ctrl v -m ctrl
---
+++ dotfiles/scripts/tmux-pde.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+tmux has-session -t "PDE" 2>/dev/null
+if [ $? != 0 ]; then
+	tmux new-session -d -n "Config" -s "PDE"
+	tmux send-keys -t 0 "cd ~/.config" "Enter" "nvim" "Enter"
+	tmux split-window -v
+	tmux resize-pane -y 15%
+	tmux select-pane -t 0
+
+	tmux new-window -n "Code"
+	tmux send-keys -t 1 "cd ~/go/src" "Enter" "nvim" "Enter"
+	tmux split-window -v
+	tmux resize-pane -y 15%
+	tmux select-pane -t 0
+
+	tmux new-window -n "Terms"
+	tmux split-window -v
+	tmux split-window -v
+	tmux select-layout even-vertical
+	tmux select-pane -t 0
+
+	tmux new-window -n "Brainhole"
+	tmux send-keys -t 0 "cd ~/Brainhole" "Enter" "nvim" "Enter"
+
+	tmux select-window -t "PDE:Code"
+fi
+
+tmux attach-session -d -t "PDE"
