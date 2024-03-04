98b4818f088af5000f73d89f92dc683c0cfa0e6f
Zach Wahrer <zach.wahrer@netography.com>
2024-03-04 14:38:50 -0700

    Nvim: Add markdown headline formatter plugin

---
+++ dotfiles/config/nvim/lua/plugins/headlines.lua
@@ -0,0 +1,31 @@
+local M = {
+	"lukas-reineke/headlines.nvim",
+	dependencies = "nvim-treesitter/nvim-treesitter",
+	ft = "markdown",
+}
+
+function M.config()
+	require("headlines").setup({
+		markdown = {
+			headline_highlights = {
+				"MarkdownHeadline1",
+				"MarkdownHeadline2",
+				"MarkdownHeadline3",
+				"MarkdownHeadline4",
+				"MarkdownHeadline5",
+				"MarkdownHeadline6",
+			},
+			bullets = {},
+			codeblock_highlight = "MarkdownCodeBlock",
+			dash_highlight = "MarkdownDash",
+			dash_string = "-",
+			quote_highlight = "MarkdownQuote",
+			quote_string = "┃",
+			fat_headlines = true,
+			fat_headline_upper_string = "▃",
+			fat_headline_lower_string = "▀",
+		},
+	})
+end
+
+return M
